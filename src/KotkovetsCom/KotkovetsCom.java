6d635df1268c6ad3757d1d7c70c8f13c951d1095
KotkovetsAndrey <kotandruha@mail.ru>
2013-12-27 07:33:13 -0800

    First comment

--- src/KotkovetsCom/KotkovetsCom.java
+++ src/KotkovetsCom/KotkovetsCom.java
@@ -14,6 +14,7 @@ public class KotkovetsCom {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Comment: next commit");
         SimpleRemoteControl remote = new SimpleRemoteControl();
         Light light = new Light();
         LightOffCommand lightOff = new LightOffCommand(light); 
